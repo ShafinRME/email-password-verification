52beb9d1637f66e226e1a1d2db6ec623db9cd1fa
ShafinRME <shafin.rmedu4@gmail.com>
2022-04-08 16:33:18 +0600

    Alhamdulillah.Email verification is done

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBCs9J0lvoJ36FR7ersu41aRgqkgXmoOGw",
+    authDomain: "email-password-auth-e0700.firebaseapp.com",
+    projectId: "email-password-auth-e0700",
+    storageBucket: "email-password-auth-e0700.appspot.com",
+    messagingSenderId: "88396753385",
+    appId: "1:88396753385:web:738496828cd903ac1a8378"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
